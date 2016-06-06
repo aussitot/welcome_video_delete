113920156bd6b18d548b7170e2fd15e53e5ad190
aussitot <contact@aussitot.net>
2016-06-06 10:07:10 +0200

    Adaptation eedomus
    Adaptation pour lancement depuis eedomus

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 //parametres
-$DayToKeep = 15; //nb de jours à garder
+$DayToKeep = $_GET['jours']; if (empty($DayToKeep)) $DayToKeep = 7;
+$xml = $_GET['xml']; if (empty($xml)) $xml = 0;
 
 //supression recursive d'un répertoire et de son contenu
 function delTree($dir) {
@@ -16,6 +17,16 @@ $dateYear = date("Y", mktime(0, 0, 0, date("m") , date("d") - $DayToKeep, date("
 $dateMonth = date("m", mktime(0, 0, 0, date("m") , date("d") - $DayToKeep, date("Y")));
 $dateDay = date("d", mktime(0, 0, 0, date("m") , date("d") - $DayToKeep, date("Y")));
 
+//echo $dateYear."/".$dateMonth."/".$dateDay;
 delTree($dateYear."/".$dateMonth."/".$dateDay);
 
+if ($xml == 1) {
+//----------- Génération du XML
+  echo '<?xml version="1.0"?>';
+  echo '<racine>';
+  echo '<camerafiles>';
+  echo '<maj>'.$dateYear."/".$dateMonth."/".$dateDay.'</maj>';
+  echo '</camerafiles>';
+  echo '</racine>';
+}
 ?>
